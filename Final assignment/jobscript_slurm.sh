b7872931e9d45cbf1981f3816ec975f47ddee120
Martijn <m.v.d.sande@student.tue.nl>
2026-03-12 16:41:56 +0100

    change experiment id and slurm output location

--- Final assignment/jobscript_slurm.sh
+++ Final assignment/jobscript_slurm.sh
@@ -5,5 +5,8 @@
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_a100
 #SBATCH --time=04:00:00
+#SBATCH --job-name=dinov3-Unet   
+#SBATCH --output=logs/%x_%j.out           
+#SBATCH --error=logs/%x_%j.err            
 
 srun apptainer exec --nv --env-file .env container.sif /bin/bash main.sh

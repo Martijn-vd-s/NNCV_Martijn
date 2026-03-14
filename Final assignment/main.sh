c819fbf04f473252d1fe4e7ec00adcf1509b91f8
Martijn <m.v.d.sande@student.tue.nl>
2026-03-14 10:51:02 +0100

    Add gardient clipping to prevent exploding gards

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,11 +4,11 @@ python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 100 \
-    --lr 0.001 \
+    --lr 0.0005 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "DINOv3 + unet-training V3" \
     --dino-fine-tune 1 \
-    --dice-weight 0.5 \
-    --ce-weight 2 \
-    --focal-weight 2 \
+    --dice-weight 0.25 \
+    --ce-weight 1 \
+    --focal-weight 1 \
--- Final assignment/train.py
+++ Final assignment/train.py
@@ -263,6 +263,10 @@ def main(args):
             loss = (args.ce_weight * crossEntropy_loss) + (args.dice_weight * dice_loss) + (args.focal_weight * focal_loss)
 
             loss.backward()
+
+            # Gradient clipping to prevent exploding gradients, especially
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+
             optimizer.step()
 
             wandb.log(

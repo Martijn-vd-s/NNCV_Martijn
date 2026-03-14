508c7b55e50d4cafe6a6fc4ad382e2041da1f230
Martijn <m.v.d.sande@student.tue.nl>
2026-03-14 11:48:10 +0100

    increase learing rate again

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,7 +4,7 @@ python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 64 \
     --epochs 100 \
-    --lr 0.0005 \
+    --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "DINOv3 + unet-training V3" \

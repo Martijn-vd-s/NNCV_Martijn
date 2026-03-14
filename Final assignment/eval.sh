0763c8ecb6968c6a18962e97061b81d397ca23e1
Martijn <m.v.d.sande@student.tue.nl>
2026-03-14 14:55:52 +0100

    eval my results

---
+++ Final assignment/eval.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
+#SBATCH --partition=gpu_a100
+#SBATCH --time=00:05:00
+#SBATCH --job-name=Eval-DINOv3 
+#SBATCH --output=logs/%x_%j_.out
+#SBATCH --error=logs/%x_%j_.err    
+
+# Execute the evaluation script inside the Apptainer container
+srun apptainer exec --nv --env-file .env container.sif /bin/bash main_eval.sh
---
+++ Final assignment/main_eval.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Starting Evaluation..."
+
+# Run the evaluation script
+python3 evaluate.py
+
+echo "Evaluation Finished! Check final_metrics.json"

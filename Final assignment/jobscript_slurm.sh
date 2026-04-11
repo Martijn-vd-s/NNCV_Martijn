972e80c69dde3feca00436ded7644f19bc3ce55a
Martijn <m.v.d.sande@student.tue.nl>
2026-04-11 18:31:48 +0200

    update .sh files

--- Final assignment/jobscript_slurm.sh
+++ Final assignment/jobscript_slurm.sh
@@ -5,7 +5,7 @@
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_a100
 #SBATCH --time=08:00:00
-#SBATCH --job-name=dinov3-Unet-V6 
+#SBATCH --job-name=dinov3-Unet-V6.1 
 #SBATCH --output=logs/%x_%j_.out
 #SBATCH --error=logs/%x_%j_.err    
 

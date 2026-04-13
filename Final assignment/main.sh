f99a0f8f567018112dcd3d36278268899ebcc869
Martijn <m.v.d.sande@student.tue.nl>
2026-04-13 20:20:24 +0200

    smaller batch?

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -2,9 +2,9 @@ wandb login
 
 python3 train.py \
     --data-dir ./data/cityscapes \
-    --batch-size 32 \
+    --batch-size 8 \
     --epochs 100 \
-    --lr 0.0003 \
+    --lr 0.0005 \
     --num-workers 18 \
     --seed 42 \
     --experiment-id "eff + unet-training V2" \

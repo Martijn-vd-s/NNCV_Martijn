af3a32e9a3ed569d37d3e20e8136e3c716a5f8be
Martijn <m.v.d.sande@student.tue.nl>
2026-04-13 20:31:30 +0200

    increase loss

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -9,7 +9,7 @@ python3 train.py \
     --seed 42 \
     --experiment-id "eff + unet-training V2" \
     --dino-fine-tune 0 \
-    --dice-weight 0.25 \
-    --ce-weight 1 \
-    --focal-weight 1 \
+    --dice-weight 0.5 \
+    --ce-weight 2 \
+    --focal-weight 2 \
     --accumulation-steps 4 \

5a6b52e5d93c0aa153850900606186c3b5c6ddfb
Martijn <m.v.d.sande@student.tue.nl>
2026-04-13 12:14:59 +0200

    change name

--- Final assignment/eval.sh
+++ Final assignment/eval.sh
@@ -4,7 +4,7 @@
 #SBATCH --gpus=1
 #SBATCH --partition=gpu_a100
 #SBATCH --time=00:45:00
-#SBATCH --job-name=Eval-DINOv3-V6.2-crf 
+#SBATCH --job-name=Eval-eff-Unet-V1 
 #SBATCH --output=logs/%x_%j_.out
 #SBATCH --error=logs/%x_%j_.err    
 

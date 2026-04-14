f6b604c8e56a15058cc6f4de3641d3e02f1d2c86
Martijn <m.v.d.sande@student.tue.nl>
2026-04-14 08:32:22 +0200

    .sh update

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -8,7 +8,6 @@ python3 train.py \
     --num-workers 18 \
     --seed 42 \
     --experiment-id "eff + unet-training V2" \
-    --dino-fine-tune 0 \
     --dice-weight 1.0 \
     --ce-weight 1 \
     --focal-weight 1.5 \
